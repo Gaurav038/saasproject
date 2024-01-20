b938272de9c3239c981b9e5ec5858d26376378ba
gaurav singh <gauravkarki038@gmail.com>
2024-01-20 18:30:41 +0530

    feat: added home page

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,21 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div className="fixed top-0 border-b bg-white w-full shadow-sm h-14 flex items-center px-4">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button size="sm" variant="outline" asChild>
+            <Link href="/sign-in">Login</Link>
+          </Button>
+          <Button size="sm" asChild >
+            <Link href="/sign-up">Get Taskify for free</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,43 @@
-const MarketPage = () => {
-    return <div>
-        MarketPage
-    </div>
-}
+import { Button } from "@/components/ui/button";
+import { Medal } from "lucide-react";
+import Link from "next/link";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
 
-export default MarketPage
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
+
+const MarketPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div
+        className={
+          cn("flex flex-col items-center justify-center",
+          headingFont.className)
+        }
+      >
+        <div className="flex border items-center bg-amber-100 text-amber-700 rounded-full p-4 shadow-sm uppercase mb-4">
+          <Medal className="h-6 w-6 mr-2" />
+          No 1 market Place
+        </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Taskify helps teams to move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          Work forward
+        </div>
+      </div>
+      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        Collaborate, manage projects, and reach new productivity peaks. From
+        high rises to the home office, the way your team works is unique -
+        accomplish it all with Taskify.
+      </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up">Get Taskify for free</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default MarketPage;
