88adc4860a68c9b6d071e26061017cbb0f97d788
gaurav singh <gauravkarki038@gmail.com>
2024-02-12 21:13:27 +0530

    feat: added board details

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,20 +1,27 @@
-import { Sidebar } from "../../_components/sidebar";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
 
-const OrganizationLayout = ({
+import { OrgControl } from "./_components/org-control";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+};
+
+const OrganizationIdLayout = ({
   children
 }: {
   children: React.ReactNode;
 }) => {
   return (
-    <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
-      <div className="flex gap-x-7">
-        <div className="w-64 shrink-0 hidden md:block">
-          <Sidebar />
-        </div>
-        {children}
-      </div>
-    </main>
+    <>
+      <OrgControl />
+      {children}
+    </>
   );
 };
 
-export default OrganizationLayout;
+export default OrganizationIdLayout;
