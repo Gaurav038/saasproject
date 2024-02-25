ff36f9dcce24db8cd7bf58b145d92c6365e5b55a
gaurav singh <gauravkarki038@gmail.com>
2024-02-25 19:50:28 +0530

    feat: board pages

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -2,14 +2,17 @@
 
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
 
-import { UpdateBoard } from "./schema";
+import { DeleteBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
 // import { createAuditLog } from "@/lib/create-audit-log";
 // import { ACTION, ENTITY_TYPE } from "@prisma/client";
+// import { decreaseAvailableCount } from "@/lib/org-limit";
+// import { checkSubscription } from "@/lib/subscription";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -20,34 +23,37 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-  const { title, id } = data;
+//   const isPro = await checkSubscription();
+
+  const { id } = data;
   let board;
 
   try {
-    board = await db.board.update({
+    board = await db.board.delete({
       where: {
         id,
         orgId,
       },
-      data: {
-        title,
-      },
     });
+
+    // if (!isPro) {
+    //   await decreaseAvailableCount();
+    // }
 
     // await createAuditLog({
     //   entityTitle: board.title,
     //   entityId: board.id,
     //   entityType: ENTITY_TYPE.BOARD,
-    //   action: ACTION.UPDATE,
+    //   action: ACTION.DELETE,
     // })
   } catch (error) {
     return {
-      error: "Failed to update."
+      error: "Failed to delete."
     }
   }
 
-  revalidatePath(`/board/${id}`);
-  return { data: board };
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
 };
 
-export const updateBoard = createSafeAction(UpdateBoard, handler);
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
